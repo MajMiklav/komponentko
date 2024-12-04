00b444e894f4cf8c8c3a85e1806de259d94ef4e0
MajMiklav <maj@lovetemple.net>
2024-12-04 22:12:03 +0100

    Dodajanje frontend pipelina

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,8 +1,1 @@
-import '@testing-library/jest-dom/extend-expect';  // Za dodatne asserte iz jest-dom
-import { configure } from '@testing-library/react';
-
-// Nastavitve za JSDOM
-configure({ testIdAttribute: 'data-testid' });
-
-// Zaženi dodatne nastavitve (če je potrebno)
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+import '@testing-library/jest-dom';
