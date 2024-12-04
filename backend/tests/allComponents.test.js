0a9bebbc6665161c731edf608dd3c4f0bd35303c
MajMiklav <maj@lovetemple.net>
2024-12-04 11:44:46 +0100

    Dodajanje pipelina

--- backend/tests/allComponents.test.js
+++ backend/tests/allComponents.test.js
@@ -1,13 +1,9 @@
-// tests/getComponentsForComputer.test.js
 const request = require('supertest');
 const app = require('../server');
 const mongoose = require('mongoose');
-const Computer = require('../models/Computer');
-const Component = require('../models/Component');
 
 describe('GET /api/computers/:id/components', () => {
   let computerId;
-  let componentIds = [];
 
   beforeAll(async () => {
     // Create a computer
@@ -17,26 +13,21 @@ describe('GET /api/computers/:id/components', () => {
     
     computerId = computerResponse.body._id;
 
-    // Add multiple components to the computer
-    const components = [
-      { name: 'RAM', description: '8GB DDR4', price: 80, user_name: 'User1' },
-      { name: 'SSD', description: '256GB SSD', price: 120, user_name: 'User2' }
-    ];
+    // Add components to the computer
+    await request(app)
+      .post(`/api/computers/${computerId}/components`)
+      .send({ name: 'RAM', description: '8GB DDR4', price: 80, user_name: 'User1' });
 
-    for (const component of components) {
-      const componentResponse = await request(app)
-        .post(`/api/computers/${computerId}/components`)
-        .send(component);
-      
-      componentIds.push(componentResponse.body._id);
-    }
+    await request(app)
+      .post(`/api/computers/${computerId}/components`)
+      .send({ name: 'SSD', description: '256GB SSD', price: 120, user_name: 'User2' });
   });
 
   afterAll(async () => {
-    // Clean up by deleting the created computer and components
-    await Component.deleteMany({ _id: { $in: componentIds } });
-    await Computer.findByIdAndDelete(computerId);
-    await mongoose.connection.close(); // Close the connection after tests
+    // Cleanup by deleting all computers and components manually
+    await mongoose.connection.collection('computers').deleteMany({});
+    await mongoose.connection.collection('components').deleteMany({});
+    await mongoose.connection.close(); // Close the connection
   });
 
   it('should return all components for the specified computer', async () => {
@@ -45,8 +36,6 @@ describe('GET /api/computers/:id/components', () => {
     // Assert that the response status code is 200
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(2); // We added 2 components
-
-    // Assert that the components returned match the ones we added
     expect(res.body[0].name).toBe('RAM');
     expect(res.body[1].name).toBe('SSD');
   });
@@ -65,8 +54,5 @@ describe('GET /api/computers/:id/components', () => {
     // Assert that the response status code is 200 and no components exist
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(0);
-
-    // Clean up
-    await Computer.findByIdAndDelete(newComputerId);
   });
 });
